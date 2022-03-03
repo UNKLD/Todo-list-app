2fde54e2c371ff2daa539e50fe94614162a6627c
Leul <leuldamtew@yahoo.com>
2022-03-03 09:14:20 -0900

    Update app.js

--- app.js
+++ app.js
@@ -126,6 +126,6 @@ app.get("/about", function (_req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
